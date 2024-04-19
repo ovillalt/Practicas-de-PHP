3fe3181419635e64efa6d4b83a9066e50c0a5435
Osvaldo Villalobos <proyectos@osvaldovillalobos.com>
2024-04-19 17:44:16 -0500

    Creando conexion a la base de datos

--- db.php
+++ db.php
@@ -1,1 +1,18 @@
-practicasphp
+<?php
+if($_SERVER["HTTP_HOST"]=="loclhost"){
+    $server="localhost";
+    $db="practicasphp";
+    $user="root";
+    $password="";
+}else{
+    $server="localhost";
+    $db="chowa_practicasphp";
+    $user="root";
+    $password="6*37J2hmn";
+}
+try{
+    $conection = new PDO("mysql:host=$server;dbname=$db",$user,$password);
+    echo "Thats great, You're connected";
+}catch(Exception $exception){
+    echo $exception->getMessage();
+}
