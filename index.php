4dc36523a1762ceb63c228f5a8b8cc2fd059a858
Osvaldo Villalobos <proyectos@osvaldovillalobos.com>
2024-04-19 19:02:51 -0500

    Separación de los archivos header y footer

--- index.php
+++ index.php
@@ -1,69 +1,20 @@
-<!doctype html>
-<html lang="es">
+<?php
+include_once ("templates/header.php");
+?>
 
-<head>
-    <title>Practicas de PHP - Osvaldo José Villalobos Torres</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-</head>
-
-<body>
-    <header>
-        <nav class="navbar navbar-expand navbar-light bg-light">
-            <ul class="nav navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#" aria-current="page">Practicas PHP <span
-                            class="visually-hidden">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Empleados</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Puestos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Usuarios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cerrar sessión</a>
-                </li>
-            </ul>
-        </nav>
-
-    </header>
-    <main>
-        <div class="p-5 mb-4 bg-light rounded-3">
-            <div class="container-fluid py-5">
-                <h1 class="display-5 fw-bold">Practicas de PHP - Osvaldo José Villalobos Torres</h1>
-                <p class="col-md-8 fs-4">
-                    Using a series of utilities, you can create this jumbotron, just
-                    like the one in previous versions of Bootstrap. Check out the
-                    examples below for how you can remix and restyle it to your liking.
-                </p>
-                <button class="btn btn-primary btn-lg" type="button">
-                    Example button
-                </button>
-            </div>
-        </div>
-
-
-    </main>
-    <footer>
-        <!-- place footer here -->
-    </footer>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Practicas de PHP - Osvaldo José Villalobos Torres</h1>
+        <p class="col-md-8 fs-4">
+            Using a series of utilities, you can create this jumbotron, just
+            like the one in previous versions of Bootstrap. Check out the
+            examples below for how you can remix and restyle it to your liking.
+        </p>
+        <button class="btn btn-primary btn-lg" type="button">
+            Ingresa/Registrate
+        </button>
+    </div>
+</div>
+<?php
+include_once("templates/footer.php");
+?>
