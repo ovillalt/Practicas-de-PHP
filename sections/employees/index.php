aff586eb8be5f135c33fae926f96488b461ef69c
Osvaldo Villalobos <proyectos@osvaldovillalobos.com>
2024-04-19 21:14:59 -0500

    Creacion de interfax de empleados

--- sections/employees/index.php
+++ sections/employees/index.php
@@ -1,7 +1,43 @@
 <?php
 include_once ("../../templates/header.php");
 ?>
-<h1>Empleados index</h1>
+<h1>Lista de empleados</h1>
+<div class="card">
+    <div class="card-header">Empleados</div>
+    <div class="card-body">
+        <div
+            class="table-responsive-sm"
+        >
+            <table
+                class="table"
+            >
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">CV</th>
+                        <th scope="col">Puesto</th>
+                        <th scope="col">Fecha de ingreso</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row">Osvaldo Villalobos</td>
+                        <td>Imagen.jpeg</td>
+                        <td>CV.pdf</td>
+                        <td>Programador Jr.</td>
+                        <td>10/08/2023</td>
+                        <td>Carta CV | Editar | Eliminar </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+    <div class="card-footer text-muted">Footer</div>
+</div>
+
 
 
 
